01d938a753ed377792cdc429377311f2c0b4e89a
Maylis Castell <mayliscastell@laposte.net>
2019-11-28 11:33:24 +0100

    add default value qd creation de réservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_123233) do
+ActiveRecord::Schema.define(version: 2019_11_28_101707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_123233) do
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.integer "total_price"
     t.bigint "equipment_id"
     t.bigint "user_id"
